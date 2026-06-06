65cc81932be18b23b49fefd65ebe35aac97b895d
antonioortegajr <antonioortegajr@gmail.com>
2026-06-05 19:21:32 -0700

    Fix #13: Image overlay on camera view

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,16 @@
 
 import { useState } from "react";
 import CameraFeed from "./components/CameraFeed";
+import ImageOverlay from "./components/ImageOverlay";
 import ImagePicker from "./components/ImagePicker";
 
 export default function Home() {
   const [selectedImage, setSelectedImage] = useState<string | null>(null);
 
-  if (selectedImage) {
-    console.log(selectedImage);
-  }
-
   return (
     <div className="relative w-full h-dvh">
       <CameraFeed />
+      {selectedImage && <ImageOverlay imageUrl={selectedImage} opacity={1} />}
       <ImagePicker onImageSelected={setSelectedImage} />
     </div>
   );
